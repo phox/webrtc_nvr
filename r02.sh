6ad7c64a11f6fc9340f8ffd0910d2100cba8d0c7
BonheyonGu <8whwhdtk8@naver.com>
2023-03-14 11:45:20 +0900

    타임 오버플로우 방지 추가

--- r02.sh
+++ r02.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmd="ffmpeg -hwaccel cuda -r 15 -i rtsp://user01:1234@10.3.129.74:12100/stream2 -c copy -f segment -segment_time 3600 -segment_format mp4 -strftime 1 -r 15 /root/nvr/record/stream2-%Y%m%d_%H%M%S.mp4"
+cmd="ffmpeg -hwaccel cuda -correct_ts_overflow 0 -r 15 -i rtsp://user01:1234@10.3.129.74:12100/stream2 -c copy -f segment -segment_time 3600 -segment_format mp4 -strftime 1 -r 15 /root/nvr/record/stream2-%Y%m%d_%H%M%S.mp4"
 cmdback="${cmd} &"
 echo $cmd
 echo $cmdback
